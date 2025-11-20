0df1c578a537d65a8b24c956d89466bfd13de413
parth2teja23 <parth2teja@gmail.com>
2025-11-20 16:04:09 +0530

    fixed react sessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,13 @@
-"use client";
+// "use client";
+
+// import { SessionProvider } from "next-auth/react";
+
+// export function Providers({ children }) {
+//   return <SessionProvider>{children}</SessionProvider>;
+// }
 
 import { SessionProvider } from "next-auth/react";
 
-export function Providers({ children }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
